b2855837131f35bb2e3f85ac1247232e15319a98
unknown <dikifahrul>
2024-01-11 19:13:18 +0700

    first commit

---
+++ storage/framework/views/db89ec9815e85cb9ca14b863b097d52e.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hasil Perhitungan Luas Persegi</title>
+</head>
+<body>
+
+<h1>Hasil Perhitungan Luas Persegi</h1>
+
+<p>Sisi: <?php echo e($sisi); ?></p>
+<p>Luas: <?php echo e($luas); ?></p>
+
+</body>
+</html>
+<?php /**PATH C:\Users\RPL SMKN 4 BOGOR\nama-proyek\resources\views/luas_persegi.blade.php ENDPATH**/ ?>
